8d0fc667dccfca9b5ac2dc1b881c2559467603fb
Abdullah Baig <mirzaabdullah3637@gmail.com>
2021-11-25 15:54:59 +0500

    testing-free-host

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
  
 //MySQLi Procedural
-$conn = mysqli_connect("localhost","root","","chat_system");
+$conn = mysqli_connect("localhost","id17990818_developer","(?%({@Jkr(9p&ikt","id17990818_chat_system");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
--- steamauth/SteamConfig.php
+++ steamauth/SteamConfig.php
@@ -1,7 +1,7 @@
 <?php
 //Version 4.0
-$steamauth['apikey'] = "CF6515A4D66E48D56E3D55B2C7D5441C"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "https://testing-fiver-project.000webhostapp.com/"; // The main URL of your website displayed in the login page
+$steamauth['apikey'] = "0C05F6A1AFAFB34944EBC3E8586F5D59"; // Your Steam WebAPI-Key found at https://steamcommunity.com/dev/apikey
+$steamauth['domainname'] = "https://project-testing-fiverr.000webhostapp.com"; // The main URL of your website displayed in the login page
 $steamauth['logoutpage'] = "index.php"; // Page to redirect to after a successfull logout (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 $steamauth['loginpage'] = "signup.php"; // Page to redirect to after a successfull login (from the directory the SteamAuth-folder is located in) - NO slash at the beginning!
 
